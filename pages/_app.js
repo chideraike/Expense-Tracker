e17056d9164ae7401a642faed496f53a814fa660
Chidera Ike <chideraike.s@gmail.com>
2023-01-10 00:44:38 +0100

    :bug: fixed bug causing it not to run

--- pages/_app.js
+++ pages/_app.js
@@ -19,12 +19,12 @@ import '../styles/global.scss'
 import '../styles/firebaseui-styling.global.scss';
 import { ThemeProvider } from '@mui/material/styles';
 import { theme } from '../styles/theme.js';
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
 export default function App({ Component, pageProps }) {
     return (
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
             <ThemeProvider theme={theme}>
                 <Component {...pageProps} />
             </ThemeProvider>
